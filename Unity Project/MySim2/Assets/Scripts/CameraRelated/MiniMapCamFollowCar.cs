4e751cd86fc38159bd9eea05c65d92abf3aee200
FJJLeon <695048014@qq.com>
2020-05-21 22:32:52 +0800

    create UI for color and depth image, add miniMap

---
+++ Unity Project/MySim2/Assets/Scripts/CameraRelated/MiniMapCamFollowCar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCamFollowCar : MonoBehaviour
+{
+    [Header("Follow Target Car")]
+    public GameObject followedCar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (followedCar == null)
+        {
+            Debug.Log("MiniMap Camera follow target car missing.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("car change to x:" + followedCar.transform.position.x + ", z: " + followedCar.transform.position.z);
+        this.gameObject.GetComponent<Transform>().position = new Vector3(followedCar.transform.position.x, 0, followedCar.transform.position.z);
+    }
+}
